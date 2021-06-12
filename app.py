74b09b8d2535cb024a746dd7e4f1cf9650127f7e
PerksofbeingVaibhav <kaustubhmishra1221@gmail.com>
2021-06-12 11:48:55 +0530

    Updating the backend part of code

--- app.py
+++ app.py
@@ -6,11 +6,6 @@ import torch
 from model import NeuralNet
 from nltk_utils import bag_of_words, tokenize
 
-chats = []
-
-repo = []
-
-
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -30,10 +25,6 @@ model_state = data["model_state"]
 model = NeuralNet(input_size, hidden_size, output_size).to(device)
 model.load_state_dict(model_state)
 model.eval()
-
-
-bot_name = "Sam"
-print("Let's chat! (type 'quit' to exit)")
 
 
 def tokk(sentence):
@@ -56,9 +47,7 @@ def tokk(sentence):
         for intent in intents['intents']:
             if tag == intent["tag"]:
                 data = random.choice(intent['responses'])
-                print(data)
 
-    print(data)            #repo.append(f"{data}")
     return data
 
 
@@ -67,43 +56,28 @@ app = Flask(__name__)
 
 @app.route("/")
 def beginning():
-    chats.clear()
-
-    repo.clear()
 
     return render_template('begin.html')
 
 
-@app.route("/index.html", methods=["GET","POST"])
+@app.route("/index.html")
 def hello ():
+
     return render_template('/index.html')
 
 
 @app.route("/get")
-#function for the bot response
-#def get_bot_response():
-#    userText = request.args.get('msg')
-#    return str(englishBot.get_response(userText))
 def hello_world(name = None):
 
     sentences = None
 
-    #while True:
-        # sentence = "do you use credit cards
-        #if request.method == 'POST':
     sentences = request.args.get("msg")
-    print(sentences)
-        #chats.append( f"{sentences}" )
-
-        #else:
-        #    sentences = "Hi!!"
-        #    chats.append( f"{sentences}" )
-
 
     loop = tokk(sentences)
 
     return str(loop)
-        #return render_template('/index.html',value = sentences, response = loop)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
